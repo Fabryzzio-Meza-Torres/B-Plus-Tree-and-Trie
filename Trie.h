f3e357c748beca80d5ee294b74fce1e977bbcd6d
Yoselyn Miranda <91152700+Yoselyn214@users.noreply.github.com>
2024-02-08 17:02:59 -0500

    Update Trie.h

--- Trie.h
+++ Trie.h
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Node
+{
+    string value;
+    vector<Node *> children;
+    Node(string value) : value(value), children(26, nullptr) {}
+};
+struct Trie
+{
+private:
+    Node *root;
+    int charL(char c){
+        return c-'a';
+    }
 
+public:
+    Trie() : root(new Node("")) {}
+    void insert(string word)
+    {
+        Node *current = root;
+        for (char c : word)
+        {
+            if (current->children[charL(c)] == nullptr)
+            {
+                current->children[charL(c)] = new Node(current->value + c);
+            }
+            current = current->children[charL(c)];
+        }
+    }
+    bool buscar(string word){
+        Node* current = root;
+        for(char c: word){
+            if(!current->children[charL(c)]){
+                return false;
+            }
+            current = current->children[charL(c)];
+        }
+        return current != nullptr && current->value == word;
+    }
+    void eliminar(string word){
+        Node *current = root;
+
+    }
+    Node *getRoot()
+    {
+        return root;
+    }
+    void print(Node *root)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        cout << root->value << endl;
+        for (int i = 0; i < 26; i++)
+        {
+            print(root->children[i]);
+        }
+    }
+};
